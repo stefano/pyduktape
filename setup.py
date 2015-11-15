2ae443223a922e77ef3734d9c90dcb4035ff1b72
Stefano <stefano.dissegna@gmail.com>
2015-11-15 13:40:08 +0100

    added missing manifest

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ extensions = [
 
 setup(
     name='pyduktape',
-    version='0.0.2',
+    version='0.0.3',
     author='Stefano Dissegna',
     description='Python integration for the Duktape Javascript interpreter',
     long_description=long_description,
